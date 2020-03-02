a9601aa90d488b0cd0eafb0825cb95317c96297e
Abdu <Abdu0901@ltkskole.dk>
2020-03-02 18:51:17 +0100

    Added base project in Csharp

---
+++ SOP-RSA-Program/SOP-RSA-Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOP_RSA_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
