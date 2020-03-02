fc52b4fbd6b3fa4146531462a571c3b4b5373f96
Abdu <Abdu0901@ltkskole.dk>
2020-03-02 20:10:25 +0100

    Added CryptoHelpers folder and RSACrypto class

---
+++ SOP-RSA-Program/SOP-RSA-Program/CryptoHelpers/RSACrypto.cs
@@ -0,0 +1,12 @@
+using System.Security.Cryptography;
+
+namespace SOP_RSA_Program.CryptoHelpers
+{
+    public class RSACrypto
+    {
+        
+        //public static byte[] Encrypt(byte[] DataToBeEncrypted)
+        
+        
+    }
+}
--- SOP-RSA-Program/SOP-RSA-Program/Program.cs
+++ SOP-RSA-Program/SOP-RSA-Program/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace SOP_RSA_Program
 {
     class Program
     {
+        // Creates byte arrays to hold the original, encrypted, and decrypted data.
+        private byte[] dataToEncrypt;
+        private byte[] dataEncrypted;
+        private byte[] dataDecrypted;
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
